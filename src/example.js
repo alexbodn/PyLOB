b00a7180ebf388b940cda3221c824130f15f0839
abraham sofer <alexbodn@gmail.com>
2023-03-22 22:33:34 +0200

    separate system from applicatuon

--- src/example.js
+++ src/example.js
@@ -1,30 +1,3 @@
-	/**
-	 Set up our output channel differently depending
-	 on whether we are running in a worker thread or
-	 the main (UI) thread.
-	*/
-	let logHtml;
-	if(self.window === self /* UI thread */){
-		console.log("Running demo from main UI thread.");
-		logHtml = function(cssClass,...args){
-			const ln = document.createElement('div');
-			if(cssClass) ln.classList.add(cssClass);
-			ln.append(document.createTextNode(args.join(' ')));
-			document.body.append(ln);
-		};
-	}
-	else{ /* Worker thread */
-		console.log("Running demo from Worker thread.");
-		logHtml = function(cssClass,...args){
-			postMessage({
-				type:'log',
-				payload:{cssClass, args}
-			});
-		};
-	}
-	const log = (...args)=>logHtml('',...args);
-	const warn = (...args)=>logHtml('warning',...args);
-	const error = (...args)=>logHtml('error',...args);
 
 
 function test_init(db, instrument) {
@@ -51,7 +24,6 @@ function test_init(db, instrument) {
 		values ('${instrument}', 'USD')
 		on conflict do nothing;
 		commit;`;
-	//log(create_traders);
 	db.exec(create_traders);
 }
 
@@ -204,80 +176,3 @@ function test_perform(lob, instrument) {
 	warn("A market ask for 40 should take all the bids and keep the remainder in the book", JSON.stringify(marketOrder2, null, '\t'));
 	lob.print(instrument);
 }
-
-
-	if(self.window!==self) /*worker thread*/{
-	/*
-		If sqlite3.js is in a directory other than this script, in order
-		to get sqlite3.js to resolve sqlite3.wasm properly, we have to
-		explicitly tell it where sqlite3.js is being loaded from. We do
-		that by passing the `sqlite3.dir=theDirName` URL argument to
-		_this_ script. That URL argument will be seen by the JS/WASM
-		loader and it will adjust the sqlite3.wasm path accordingly. If
-		sqlite3.js/.wasm are in the same directory as this script then
-		that's not needed.
-
-		URL arguments passed as part of the filename via importScripts()
-		are simply lost, and such scripts see the self.location of
-		_this_ script.
-	*/
-	let sqlite3Js = 'sqlite3.js';
-	const urlParams = new URL(self.location.href).searchParams;
-	if(urlParams.has('sqlite3.dir')){
-		sqlite3Js = urlParams.get('sqlite3.dir') + '/' + sqlite3Js;
-	}
-	importScripts(sqlite3Js);
-	}
-	self.sqlite3InitModule({
-		// We can redirect any stdout/stderr from the module
-		// like so...
-		print: /*log*/()=>{},
-		printErr: /*console.error*/()=>{}
-	}).then(function(sqlite3){
-		//log("Done initializing. Running demo...");
-		try {
-			let myurl = window.location.href;
-			let mydir = myurl.substring(0, myurl.lastIndexOf('/'));
-			const oo = sqlite3.oo1/*high-level OO API*/;
-			const db = new oo.DB("/mydb.sqlite3",'ct');
-			// isolation_level: null
-			fetchText('create_db', mydir + '/create_lob.sql').then(
-				function(sql) {
-					//sql[1] = sql[1].replaceAll('begin transaction;', '').replaceAll('commit;');
-					//log(sql[1]);
-					//try
-					db./*transaction(
-						function(D) {
-							D.*/exec({
-								sql: /*'select 1; select 2;',*/sql[1],
-								//rowType: 'stmt',
-								/*callback: result => {
-								log(result);
-								//D.exec("select * from instrument;");
-								//let ob = new OrderBook(mydir + '/PyLOB', fetchText, D);
-								//return true;
-							}*/
-					});
-					//finally
-					db.exec({
-						sql: "select * from instrument;",
-						rowMode: 0,
-						callback: result=>log(result)
-					});
-					let ob = new OrderBook(mydir + '/PyLOB', fetchText, db);
-					ob.init().then(
-						obj => {
-							log(obj);
-							let instrument = 'FAKE';
-							test_init(db, instrument);
-						
-							test_perform(ob, instrument);
-						}
-					); 
-			/*});*/
-		});
-	}
-	catch(e){
-		console.error("Exception:",e.message);
-	}
-	});
